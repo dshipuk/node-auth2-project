fdbe23326be825637b8c36b7e60e62a9ab404754
unknown <dshipuk@yahoo.com>
2022-05-24 12:48:48 -0700

    Instaled deps created find() function

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,6 +1,6 @@
 const db = require('../../data/db-config.js');
 
-function find() {
+async function find() {
   /**
     You will need to join two tables.
     Resolves to an ARRAY with all users.
@@ -18,6 +18,9 @@ function find() {
       }
     ]
    */
+  return await db("users")
+    .join("roles", "users.role_id", "roles.role_id")
+    .select("user_id", "username", "role_name")
 }
 
 function findBy(filter) {
